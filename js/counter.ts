156618367caa12a8ae65d2bb8e97d9861e35815f
farneses <latriasouls@protonmail.com>
2024-01-29 22:37:36 -0300

    typescript

---
+++ js/counter.ts
@@ -0,0 +1,7 @@
+var count = 1;
+
+while(count <= 36){
+    console.log(count);
+
+    count = count + 1;
+}
---
+++ js/happynewyear.ts
@@ -0,0 +1,10 @@
+var count = 10;
+
+while(count >= 0){
+    console.log(count);
+   
+    count = count - 1;
+
+}
+
+console.log("Happy New Year!");
